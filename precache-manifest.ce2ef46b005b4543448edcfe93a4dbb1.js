95eac813ba120da03370cf47ed76079398110a03
kazugitgit <qiandaoyisui@gmail.com>
2020-07-10 02:21:57 +0900

    Updates

---
+++ precache-manifest.ce2ef46b005b4543448edcfe93a4dbb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1581f61048d72689d046b5be0346dad",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "46f03fe5d001a05fde85",
+    "url": "/Todo-App/static/css/main.a99a5fc6.chunk.css"
+  },
+  {
+    "revision": "148de357a9925513a8ea",
+    "url": "/Todo-App/static/js/2.583593db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.583593db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f03fe5d001a05fde85",
+    "url": "/Todo-App/static/js/main.f5fb5d61.chunk.js"
+  },
+  {
+    "revision": "7e8d4f4d208cce7a496f",
+    "url": "/Todo-App/static/js/runtime-main.723757e4.js"
+  }
+]);
